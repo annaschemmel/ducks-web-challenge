1cd0475ecd5a6d3b1ffc1d75894bf431b2c00d17
Anna Schemmel <anna.schemmel@gmx.de>
2025-07-31 23:04:26 +0200

    Fix: Challenge dark mode

--- js-dom-events/js-dom-and-events_dark-mode/js/index.js
+++ js-dom-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,18 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const buttonDark = document.querySelector('[data-js="dark-mode-button"]');
+buttonDark.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
