0f0a695b13a25d9b05c26e5eb5d214d59892d916
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-13 12:58:57 +0200

    Fix: Challenge create new post

--- js-createelement/js-createelement_social-media-post/js/index.js
+++ js-createelement/js-createelement_social-media-post/js/index.js
@@ -1,3 +1,5 @@
+//const { createElement } = require("react");
+
 console.clear();
 
 function handleLikeButtonClick(event) {
@@ -10,3 +12,33 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const body = document.getElementsByTagName("body")[0];
+//const body = document.querySelector('[data-js="body"]');
+console.log(body);
+
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+newPost.setAttribute("data-js", "like-button-2");
+
+const postContent = document.createElement("p");
+postContent.classList.add("post__content");
+postContent.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+
+const footer = document.createElement("footer");
+footer.classList.add("post__footer");
+
+const userName = document.createElement("span");
+userName.classList.add("post__username");
+userName.textContent = "@username";
+
+const postButton = document.createElement("button");
+postButton.classList.add("post__button");
+postButton.setAttribute("type", "button");
+postButton.setAttribute("data-js", "like-button");
+postButton.textContent = "♥ Like";
+
+footer.append(userName, postButton);
+newPost.append(postContent, footer);
+body.append(newPost);
