d75f3f241901aa864f0a1eca88207631dbe0af5e
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-19 13:02:53 +0200

    Add: Return variable for data

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -4,14 +4,16 @@ const url = "https://swapi.py4e.com/api/people";
 
 async function fetchData() {
   const response = await fetch(url);
-  console.log("response: ", response);
-
+  //   console.log("response: ", response);
   const data = await response.json(); //convert from json to js
-  console.log("data:", data);
-  console.log("data.result: ", data.results);
-  console.log("data.result (r2d2): ", data.results[2]);
-  console.log("data.result (r2d2 eyecolor ): ", data.results[2]["eye_color"]);
   return data;
 }
 
-fetchData();
+const characters = await fetchData();
+console.log("data:", characters);
+console.log("data.result: ", characters.results);
+console.log("data.result (r2d2): ", characters.results[2]);
+console.log(
+  "data.result (r2d2 eyecolor ): ",
+  characters.results[2]["eye_color"]
+);
