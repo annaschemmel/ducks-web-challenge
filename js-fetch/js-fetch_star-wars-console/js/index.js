548cd51bc6e78f25328f7ee501f50d3c2ac45e6d
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-19 12:28:53 +0200

    Fix: Challenge starwars console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,16 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  console.log("response: ", response);
+
+  const data = await response.json(); //convert from json to js
+  console.log("data:", data);
+  console.log("data.result: ", data.results);
+  console.log("data.result (r2d2): ", data.results[2]);
+  console.log("data.result (r2d2 eyecolor ): ", data.results[2]["eye_color"]);
+  return data;
+}
 
 fetchData();
