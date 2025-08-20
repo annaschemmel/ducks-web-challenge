d2e3af8ec544daec8033007f0b9f3cc4d9a55272
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-20 16:04:10 +0200

    Fix: Challenge stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,22 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
+  for (let counter = 0; counter < 5; counter++) {
+    const star = document.createElement("img");
+    if (counter < filledStars) {
+      star.src = "./assets/star-filled.svg";
+    } else {
+      star.src = "./assets/star-empty.svg";
+    }
+    starContainer.append(star);
+  }
 
   // --^-- write or modify code above this line --^--
 }
 
-renderStars();
+renderStars(1);
