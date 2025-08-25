ad57c68248a748078823dbe69d30bcd5221c7bec
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-25 11:38:11 +0200

    Complete: Challenge attributes

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,22 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">This is the headline</h2>
+      <label htmlFor="articleInput">Label </label>
+      <input type="text" id="articleInput" />
+      <a
+        className="article__link"
+        target="blank"
+        href="https://github.com/neuefische/ducks-hh-wd-25-2/blob/main/sessions/react-basics/challenges-react-basics.md"
+      >
+        Click me
+      </a>
+    </article>
+  );
 }
