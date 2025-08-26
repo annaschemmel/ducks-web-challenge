eaf026af6f79b06701fb36b524d82d9bd35038aa
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-26 15:50:53 +0200

    Complete: Challenge emojis

--- react-state-1/react-state-1_emoji-checker/src/App.jsx
+++ react-state-1/react-state-1_emoji-checker/src/App.jsx
@@ -1,12 +1,17 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let code = "?";
+  //let code = "?";
+  const [code, setCode] = useState("");
 
   const validCode = "🐡🐠🐋";
 
   function handleClick(emoji) {
-    console.log(emoji);
+    // console.log(emoji);
+    // console.log(code);
+    console.log(code);
+    setCode(code + emoji);
     console.log(code);
   }
 
@@ -49,6 +54,7 @@ export default function App() {
         type="button"
         onClick={() => {
           console.log("Reset Code!");
+          setCode("");
         }}
       >
         Reset
