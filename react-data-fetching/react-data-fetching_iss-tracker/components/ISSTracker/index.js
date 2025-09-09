401e8cc2c6530c8bfd696aa38ea28b17e2560e64
Anna Schemmel <anna.schemmel@gmx.de>
2025-09-09 22:45:03 +0200

    Complete: Challenge iss tracker

--- react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,45 +1,51 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 
-const URL = "https://api.wheretheiss.at/v1/satellites/25544";
-
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+  // const [coords, setCoords] = useState({
+  //   longitude: 0,
+  //   latitude: 0,
+  // });
+  // ^ we don't need this anymore since we are getting the information from useSWR (down below)
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const {
+    data: ISSCoords,
+    error,
+    isLoading,
+  } = useSWR("https://api.wheretheiss.at/v1/satellites/25544");
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  // async function getISSCoords() {
+  //   try {
+  //     const response = await fetch(URL);
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setCoords({ longitude: data.longitude, latitude: data.latitude });
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     getISSCoords();
+  //   }, 5000);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  const { longitude, latitude } = ISSCoords;
 
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
-      <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
-      />
+      <Map longitude={longitude} latitude={latitude} />
+      <Controls longitude={longitude} latitude={latitude} />
     </main>
   );
 }
--- react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
+++ react-data-fetching/react-data-fetching_iss-tracker/pages/_app.js
@@ -1,10 +1,28 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+import { SWRConfig } from "swr";
+
+/* SWRConfig for the whole APP
+We want to config swr globally for all pur app.
+We don't want to have to declare the fetcher function every time we want to use it.
+I want to declare the fetcher once and that all.
+*/
+
+//const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+async function fetcher(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Failed to fetch");
+  return await response.json();
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher, refreshInterval: 1000 }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
