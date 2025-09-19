fcfa683d5b309aa275043610dd9cfc6ad33716af
Anna Schemmel <anna.schemmel@gmx.de>
2025-09-19 11:19:14 +0200

    Add: create method

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -11,5 +11,12 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "POST") {
+    const productData = request.body;
+    await Product.create(productData);
+
+    return response.status(201).json({ status: "Product created" });
+  }
+
   response.status(405).json({ status: "Method not allowed." });
 }
